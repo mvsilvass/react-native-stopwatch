d60ef6ca4be2abbf57d904410fc1b647dd24adf9
Maria Vitoria <mvsilvasoaress@gmail.com>
2024-09-29 16:36:18 -0300

    Adiciona TimerButtons

---
+++ src/components/Timer/TimerButtons.js
@@ -0,0 +1,26 @@
+import { SafeAreaView, View, StyleSheet } from 'react-native';
+import { useContext } from 'react';
+
+import Button from '../Buttons/Button';
+import PlayPauseButton from '../Buttons/PlayPauseButton';
+import { TimerContext } from './../../contexts/TimerContext';
+
+export default function TimerButtons() {
+  const { startTimer, stopTimer, resetTimer, isTimer, hasStarted } = useContext(TimerContext);
+
+  return (
+    <SafeAreaView>
+      <View style={styles.buttonsContainer}>
+        <PlayPauseButton isTimer={isTimer} startTimer={startTimer} stopTimer={stopTimer} />
+        {hasStarted && <Button icon="restart" onPress={resetTimer} />}
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  buttonsContainer: {
+    flexDirection: 'row',
+    gap: 10,
+  },
+});
