25dbc164bc13dabcbf168de1194e15c5c554fa14
Maria Vitoria <mvsilvasoaress@gmail.com>
2024-09-29 16:42:46 -0300

    Update Metrics.js

--- src/utils/Metrics.js
+++ src/utils/Metrics.js
@@ -2,9 +2,9 @@ import { Dimensions } from 'react-native';
 
 const { width, height } = Dimensions.get('window');
 
-const metrics = {
+const Metrics = {
   screenWidth: width,
   screenHeight: height,
 };
 
-export default metrics;
+export default Metrics;
