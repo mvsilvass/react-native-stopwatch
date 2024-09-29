a1389b8754ff35973eae32c968ae9b836e5261fb
Maria Vitoria <mvsilvasoaress@gmail.com>
2024-09-29 16:14:59 -0300

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,18 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@components': './src/components',
+            '@contexts': './src/contexts',
+            '@utils': './src/utils',
+          },
+        },
+      ],
+    ],
   };
 };
