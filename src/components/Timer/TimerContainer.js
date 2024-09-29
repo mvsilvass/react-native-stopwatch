67163e97e36e722f43495c0cafe8706090554e64
Maria Vitoria <mvsilvasoaress@gmail.com>
2024-09-29 16:49:46 -0300

    Update TimerContainer.js

--- src/components/Timer/TimerContainer.js
+++ src/components/Timer/TimerContainer.js
@@ -1,12 +1,12 @@
 import { SafeAreaView, StyleSheet } from 'react-native';
 
-import Time from './TimeDisplay';
+import TimeDisplay from './TimeDisplay';
 import TimerButtons from './TimerButtons';
 
 export default function TimerContainer() {
   return (
     <SafeAreaView style={styles.timerContainer}>
-      <Time />
+      <TimeDisplay />
       <TimerButtons />
     </SafeAreaView>
   );
