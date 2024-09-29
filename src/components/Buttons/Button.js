164398c55358a321cae6eadced04bc3bfee03084
Maria Vitoria <mvsilvasoaress@gmail.com>
2024-09-29 16:39:59 -0300

    Update Button.js

--- src/components/Buttons/Button.js
+++ src/components/Buttons/Button.js
@@ -2,9 +2,9 @@ import { TouchableOpacity, Image, StyleSheet } from 'react-native';
 import PropTypes from 'prop-types';
 
 const icons = {
-  pause: require('./../../assets/pause.png'),
-  play: require('./../../assets/play.png'),
-  restart: require('./../../assets/restart.png'),
+  pause: require('@assets/pause.png'),
+  play: require('@assets/play.png'),
+  restart: require('@assets/restart.png'),
 };
 
 export default function Button({ icon, onPress }) {
@@ -24,7 +24,7 @@ const styles = StyleSheet.create({
     button: {
         flex: 1,
         alignItems: 'center',
-        padding: '4%',
+        padding: '5%',
         backgroundColor: '#6A9C89',
         borderRadius: 10,
         justifyContent: 'center',
